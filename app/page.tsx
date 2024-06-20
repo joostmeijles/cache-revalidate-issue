095e7958de215bdbaed582a0bc73ca69728fcc4c
Joost Meijles <joost.meijles@avivasolutions.nl>
2024-06-20 19:01:09 +0200

    Init

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+/** Add your relevant code here for the issue to reproduce */
+export async function generateMetadata() {
+  console.log("Start fetch");
+  const res = await fetch("http://worldtimeapi.org/api/timezone/Europe/Amsterdam", {
+    next: {
+      tags: ['my-cachetag']
+    }
+  });
+  console.log("Fetch completed");
+
+  const data = await res.json();
+
+  return {
+    title: data.unixtime.toString()
+  }
+}
+
+export default function Home() {
+  return null;
+}
